31778152668ff32403ed921642b4f35099784c00
NMeyer8386 <nathaniel.meyer@lethbridgecollege.ca>
2023-03-06 15:45:49 -0700

    Made comments for how to design the app

--- App.js
+++ App.js
@@ -1,13 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
+import { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class SoundButton extends Component {
+
+  /*
+                        Ideas for Assign4:
+------------------------------------------------------------------------
+    Create a default button class for both recording and sounds,
+  and call them all on one render instead of rendering all the buttons 
+  individually. Use callbacks to identify the button that is pressed
+
+    Load sounds on ComponentDidMount(), use example from expo/sounds/
+  to configure hold to record, press to play.
+  */
+
+  render(){
+    return (
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
